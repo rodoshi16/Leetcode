714012f99b5d17d6f252f05bd5abdadadb31de43
Rodoshi Mondal <165502326+rodoshi16@users.noreply.github.com>
2026-03-23 01:03:44 -0400

    Time: 8 ms (12.97%), Space: 20.6 MB (39.96%) - LeetHub

---
+++ 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.py
@@ -0,0 +1,26 @@
+from collections import defaultdict
+class Solution:
+    def allPathsSourceTarget(self, graph: List[List[int]]) -> List[List[int]]:
+        
+        d = defaultdict(list)
+        for i in range(len(graph)):
+            d[i].extend(graph[i])
+            
+        
+        paths = []
+        path = []
+        
+        def dfs(node):
+            path.append(node)
+            
+            if node == len(graph) - 1:
+                paths.append(path.copy())
+        
+            for nei in d[node]:
+                if nei not in path:
+                    dfs(nei)
+            path.pop()
+            
+        dfs(0)
+        return paths 
+       
