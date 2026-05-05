02728cc8622c60502c9536511d6d78b091650f63
Rodoshi Mondal <165502326+rodoshi16@users.noreply.github.com>
2026-05-05 09:45:35 -0400

    Time: 3 ms (82.71%), Space: 20.6 MB (23.37%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -3,45 +3,32 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        #input: nums-> list of integers
-        # move all zeros at the end while maintaing order
-        # brute: scan and if zero -> pop and append zero at end 
 
-        # i = 0 
-        # while i < len(nums):
-        #     if nums[i] == 0:
-        #         nums.pop(i)
-        #         nums.append(0)
-        #     i += 1 
+        #naive: scan until zero, pop and append 
+        # 2 pointer 
         
+        [0,1,0,3,12]
+        #everything on left -> non zero
 
-        #modifying loop while iterating
-        #edge cases: [0, 0, 8, 1, 2] (move to the next index but next index is pushed down)
-        # pop shifts everything left but index still moves right
-
-        left = 0 
-        right = len(nums) - 1 
+        #RIGHT pointer will always move forward 
 
         left = 0 
-        right = 1 
-
-        while right < len(nums):
-            if nums[left] == 0 and nums[right] != 0:
+        for right in range(len(nums)):
+            if nums[right] != 0:
                 nums[left], nums[right] = nums[right], nums[left]
                 left += 1 
-                right += 1 
-            elif nums[left] == 0 and nums[right] == 0:
-                right += 1 
-            else: 
-                left += 1 
-                right += 1 
 
 
-        
-
-        
-        
-        
 
 
-        
+
+
+
+
+
+
+
+
+
+
+      
