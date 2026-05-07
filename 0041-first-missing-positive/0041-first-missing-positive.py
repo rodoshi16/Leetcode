a7b07439803d7ae6b8d492fd1dbdf8a7cc481960
Rodoshi Mondal <165502326+rodoshi16@users.noreply.github.com>
2026-05-07 12:07:19 -0400

    Time: 63 ms (18.58%), Space: 31.1 MB (27.29%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.py
+++ 0041-first-missing-positive/0041-first-missing-positive.py
@@ -1,17 +1,30 @@
 class Solution:
     def firstMissingPositive(self, nums: List[int]) -> int:
-        #list: unsorted 
-        #output: smallest positive integer not present in nums
+        #turn n+1
 
-        #iteration to make a hashmap + iterate through hashmap 
+        # take ele and mark its position negative 
+        # work with abs and ignore > n 
+        # scan to find first missing positive 
 
 
-        d = {}
-        for ele in nums:
-            if ele not in d:
-                d[ele] = 1 
-    
-        for i in range(1, len(nums) + 2):
-            if i not in d:
-                return i 
+        for i in range(len(nums)):
+            if nums[i] <= 0:
+                nums[i] = len(nums) + 1 
+        
+
+        for i in range(len(nums)):
+            val = abs(nums[i])
+            if val <= len(nums):
+                if nums[val-1] > 0: 
+                    nums[val-1] = - nums[val-1]
+        
+        for i in range(len(nums)):
+            if nums[i] > 0:
+                return i+1
+        
+        return len(nums)+1
+
+           
+
+
         
