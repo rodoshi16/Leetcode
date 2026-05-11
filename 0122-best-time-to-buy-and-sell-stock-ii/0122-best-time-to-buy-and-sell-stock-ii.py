0a13ca14d3772e518a0ab4b7f11f0f658dae5a1f
Rodoshi Mondal <165502326+rodoshi16@users.noreply.github.com>
2026-05-11 14:25:15 -0400

    Time: 3 ms (61.08%), Space: 20.3 MB (85.12%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
@@ -1,27 +1,5 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        #can hold at most one share of stock at any one time
-        # u can sell/buy stock multiple times on same day (never more than one share of stock)
-
-        #[7,1,5,3,6,4]
-        # buy: when price is less
-        # sell: when price is high
-
-        # if prices[i] < buy 
-        # buy =  math.inf
-        # sell = 0 
-        # profit = 0 
-        # for i in range(len(prices)):
-        #     if prices[i] < buy:
-        #         buy = prices[i]
-        #     elif prices[i] > buy:
-        #         sell = prices[i]
-        #         profit += (sell - buy)
-        #         buy = math.inf
-        #         sell = 0
-        # return profit 
-
-
         profit = 0 
         for i in range(1, len(prices)):
             if prices[i] > prices[i-1]:
