ea61f6a2adf48890f80bd9950eeba95a282e3e04
Rodoshi Mondal <165502326+rodoshi16@users.noreply.github.com>
2026-05-12 23:37:12 -0400

    Time: 33 ms (61.55%), Space: 21.7 MB (85.2%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
@@ -1,6 +1,6 @@
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
-        d = {}
+        d = {0:1}
         n = 0
         curr = 0 
         for i in range(len(nums)):
@@ -8,13 +8,12 @@ class Solution:
             c = curr - k
             if c in d:
                 n += d[c]
-            if curr == k:
-                n += 1 
             if curr not in d: 
                 d[curr] = 1
             else:
                 d[curr] += 1
         return n 
+
 
         #[1,1,1]
         #[0,1,2,2,3], 2
