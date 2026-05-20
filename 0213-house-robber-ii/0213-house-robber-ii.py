b9733825a89be3863b1823fb35140db61d471618
Rodoshi Mondal <165502326+rodoshi16@users.noreply.github.com>
2026-05-19 23:47:11 -0400

    Time: 0 ms (100%), Space: 19.2 MB (68.55%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.py
+++ 0213-house-robber-ii/0213-house-robber-ii.py
@@ -3,21 +3,25 @@ class Solution:
         if len(nums) == 1:
             return nums[0]
 
-        def helper(nums: List[int]) -> int:
-            if not nums:
-                return 0
+        def helper(nums):
             if len(nums) == 1:
                 return nums[0]
-
+            elif len(nums) == 2:
+                return max(nums[0], nums[1])
+            
             dp = [0] * len(nums)
             dp[0] = nums[0]
             dp[1] = max(nums[0], nums[1])
 
             for i in range(2, len(nums)):
-                dp[i] = max(dp[i - 1], nums[i] + dp[i - 2])
-
+                dp[i] = max(dp[i-1], dp[i-2] + nums[i])
+            
             return dp[-1]
-        return max(helper(nums[1:]),
-                   helper(nums[:-1]))
+        
+        return max(helper(nums[1:]), helper(nums[:-1]))
+        
 
-    
+
+
+
+        
