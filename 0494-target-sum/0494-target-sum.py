f084221291b743267f993b10299b0308c0b03172
Rodoshi Mondal <165502326+rodoshi16@users.noreply.github.com>
2026-05-31 17:22:10 -0400

    Time: 107 ms (65.6%), Space: 19.5 MB (59.39%) - LeetHub

--- 0494-target-sum/0494-target-sum.py
+++ 0494-target-sum/0494-target-sum.py
@@ -1,7 +1,5 @@
 class Solution:
     def findTargetSumWays(self, nums: List[int], target: int) -> int:
-        dp = [nums[0], -nums[0]]
-        c = 0 
 
         if len(nums) == 1:
             if nums[0] == 0:
@@ -11,61 +9,23 @@ class Solution:
             else:
                 return 0 
 
+        if nums[0] == 0:
+            dp = {0: 2}
+        else: 
+            dp = {nums[0]:1, -nums[0]: 1}
+
+
         for i in range(1, len(nums)):
-            s = []
+            s = {}
             for ele in dp:
-                s.append(ele + nums[i])
-                s.append(ele - nums[i])
+                a = ele + nums[i]
+                b = ele - nums[i]
+                s[a] = s.get(a, 0) + dp[ele]
+                s[b] = s.get(b, 0) + dp[ele]
             
             dp = s
-
-            if i == len(nums) -1:
-                for ele in s:
-                    if ele == target:
-                        c += 1 
         
-        return c 
-
-        
-
-
-
-
-            
-
-
-
-        
-        # dp = [nums[0], -nums[0]]
-
-        # if len(nums) == 1:
-        #     if nums[0] == 0:
-        #         return 2
-        #     elif nums[0] == target or -nums[0] == target:
-        #         return 1
-        #     else:
-        #         return 0
-
-
-
-        # c = 0 
-
-        # for i in range(1, len(nums)):
-        #     s = []
-        #     for ele in dp:
-        #         s.append(ele + nums[i])
-        #         s.append(ele - nums[i])
-        
-        #     dp = s 
-        
-
-        #     if i == len(nums) -1:
-        #         for ele in dp:
-        #             if ele == target:
-        #                 c += 1 
-
-        # return c
-                
-            
-
-        
+        if target in dp:
+            return dp[target]
+        else:
+            return 0
