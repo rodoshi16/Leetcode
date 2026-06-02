697758b9bc56a132b68d38b5807dd7a66974b07b
Rodoshi Mondal <165502326+rodoshi16@users.noreply.github.com>
2026-06-02 14:01:36 -0400

    Time: 0 ms (100%), Space: 19.3 MB (19.55%) - LeetHub

--- 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.py
+++ 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.py
@@ -5,21 +5,10 @@
 #         self.next = next
 class Solution:
     def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        if head is None:
-            return None
-        elif head.next is None:
-            return head
-
-
-        slow = None
-        fast = head 
+        slow = head
+        fast = head
 
         while fast and fast.next is not None:
-            if slow is None:
-                slow = head
-            else:
-                slow = slow.next 
-            fast = fast.next.next
-        
-        return slow.next
-        
+            slow = slow.next 
+            fast = fast.next.next 
+        return slow
