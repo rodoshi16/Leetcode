764cefdfab210ea368f4df92577ccc7f80e4a10c
Rodoshi Mondal <165502326+rodoshi16@users.noreply.github.com>
2026-06-07 13:14:01 -0400

    Time: 511 ms (63.55%), Space: 19.6 MB (77.62%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -1,57 +1,15 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        dp = [float('inf')] * (amount + 1)
-        dp[0] = 0
+        dp = [math.inf] * (amount+1)
+        dp[0] = 0 
 
-        for a in range(1, amount + 1):
+        for i in range(1, amount+1):
             for c in coins:
-                if a >= c:
-                    dp[a] = min(dp[a], dp[a - c] + 1)
+                if i - c >= 0:
+                    dp[i] = min(dp[i], dp[i-c]+1)
 
-        return dp[amount] if dp[amount] != float('inf') else -1
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        # if amount == 0:
-        #     return 0
-
-        # if len(coins) == 1:
-        #     if amount == coins[0]:
-        #         return 1
-        #     elif amount != coins[0] and amount == 0:
-        #         return 0
-        #     elif amount % coins[0] == 0:
-        #         return amount // coins[0]
-        #     else:
-        #         return -1
-
-        # i = 1 
-        # dp = [0]
-        # while i < 90: 
-        #     s = set()
-        #     for ele in dp:
-        #         for c in coins:
-        #             a = ele + c
-        #             s.add(a)
-        #             if a == amount:
-        #                 return i
-        #     i += 1
-        #     dp = s
-        # return -1
-
-
-
-
+        if dp[amount] == math.inf:
+            return -1
+        
+        return dp[amount]
+        
