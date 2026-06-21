f574f82355b6a2bbafdd91c8caebc4204c3b0c0f
Rodoshi Mondal <165502326+rodoshi16@users.noreply.github.com>
2026-06-20 23:47:35 -0400

    Time: 129 ms (21.39%), Space: 50.7 MB (18.49%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.py
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.py
@@ -11,16 +11,13 @@ class Solution:
         if root is None:
             return None
 
-        # if we found p or q, return it
         if root == p or root == q:
             return root
 
         left = self.lowestCommonAncestor(root.left, p, q)
         right = self.lowestCommonAncestor(root.right, p, q)
 
-        # p and q found in different branches
         if left and right:
             return root
 
-        # otherwise return whatever is not null
         return left if left else right
