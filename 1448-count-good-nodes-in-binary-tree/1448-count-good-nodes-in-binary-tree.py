09ab9d3d6a89b2ffba02eb20b48cdbf2d70f5105
Rodoshi Mondal <165502326+rodoshi16@users.noreply.github.com>
2026-07-03 15:18:25 -0400

    Time: 133 ms (39.03%), Space: 31.9 MB (69.28%) - LeetHub

--- 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.py
+++ 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.py
@@ -7,27 +7,22 @@
 class Solution:
     def goodNodes(self, root: TreeNode) -> int:
         #good: if in the path from root to X no nodes greater than X
+        # root counts as 1 and check if its crossed the max seen so far 
 
         if root is None:
-            return 0
+            return 0 
         
-        m  = root.val
-     
+        m = root.val 
+
         def helper(root, m):
             if root is None:
-                return 0
-            
+                return 0 
             count = 0 
             if root.val >= m:
-                count = 1
-                
-            m = max(root.val, m)
+                count = 1 
+            
+            m = max(m, root.val)
 
             return count + helper(root.left, m) + helper(root.right, m)
-           
+        
         return helper(root, m)
-
-
-
-
-                
