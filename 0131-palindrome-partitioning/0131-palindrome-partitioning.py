57042474c9647498afd264c96da827d2dfb74ac4
Rodoshi Mondal <165502326+rodoshi16@users.noreply.github.com>
2026-07-05 16:24:08 -0400

    Time: 55 ms (16.29%), Space: 34.2 MB (31.8%) - LeetHub

---
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.py
@@ -0,0 +1,43 @@
+class Solution:
+    def partition(self, s: str) -> List[List[str]]:
+
+        def isp(s):
+            left = 0 
+            right = len(s) - 1 
+            
+            while left < right:
+                if not s[left].isalnum():
+                    left += 1 
+                elif not s[right].isalnum():
+                    right -=1 
+                
+                else:
+                    if s[right].lower() != s[left].lower():
+                        return False 
+                
+                    left += 1 
+                    right -= 1 
+            
+            return True 
+    
+        
+        res = []
+        def backtrack(k, current):
+            if len(k) == 0:
+                res.append(current[:])
+                return
+
+            for i in range(len(k)):
+                if isp(k[0:i+1]):
+                    current.append(k[0:i+1])
+                    backtrack(k[i+1:], current)
+
+                    current.pop()
+
+        backtrack(s, [])
+        return res
+
+           
+            
+
+       
