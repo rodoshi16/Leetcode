e67dfcf9dd7bfbf04a1c933cbc37714144455dc0
Rodoshi Mondal <165502326+rodoshi16@users.noreply.github.com>
2026-07-05 21:41:21 -0400

    Time: 8 ms (81.38%), Space: 19.6 MB (68.59%) - LeetHub

--- 0051-n-queens/0051-n-queens.py
+++ 0051-n-queens/0051-n-queens.py
@@ -1,104 +1,50 @@
 class Solution:
     def solveNQueens(self, n: int) -> List[List[str]]:
-        board = [["."] * n for _ in range(n)]
 
-        r = []
-        c = []
-        d = []
         res = []
+        lst = []
+        for i in range(n):
+            lst.append(["."]*n)
 
-        def backtrack(i, j, queens):
-            if queens == n:
-                res.append(["".join(row) for row in board])
-                return
+        c = set()
+        d1 = set()
+        d2 = set()
 
-            if i == n:
-                return
+        def backtrack(q, row, lst):
+            if n == q:
+                res.append(["".join(row) for row in lst])
+                return 
+            
+            for col in range(n):
+                if col not in c and (row+col) not in d1 and (row-col) not in d2:
+                    lst[row][col] = "Q"
+                    c.add(col)
 
-            # move to next cell
-            next_i = i
-            next_j = j + 1
+    
+                    d1.add(row+col)
+                    d2.add(row-col)
 
-            if next_j == n:
-                next_i += 1
-                next_j = 0
+                    backtrack(q+1, row+1, lst)
 
-            # option 1: don't place queen here
-            backtrack(next_i, next_j, queens)
+                    lst[row][col] = "."
+                    c.remove(col)
+                    d1.remove(row+col)
+                    d2.remove(row-col)
 
-            # option 2: place queen here
-            if (i not in r) and (j not in c) and ((i, j) not in d):
-                board[i][j] = "Q"
-                r.append(i)
-                c.append(j)
 
-                old_len = len(d)
-
-                # mark diagonals
-                x, y1, y2 = i, j, j
-                while x + 1 < n:
-                    x += 1
-                    y1 += 1
-                    y2 -= 1
-                    d.append((x, y1))
-                    d.append((x, y2))
-
-                backtrack(next_i, next_j, queens + 1)
-
-                # undo diagonals
-                while len(d) > old_len:
-                    d.pop()
-
-                # undo queen
-                board[i][j] = "."
-                r.pop()
-                c.pop()
-
-        backtrack(0, 0, 0)
+        backtrack(0, 0, lst)
         return res
-        # lst = []
-        # for _ in range(n):
-        #     lst.append(["."]* n)
-            
-        # r = []
-        # c = []
-        # d = []
-        # res = []
-        # def backtrack(i, j, queens):
-        #     if queens == n:
-        #         res.append(lst[:])
-        #         return 1
-            
-        #     if i == n:
-        #         return 
-
-        #     for i in range(len(lst)):
-        #         for j in range(len(lst)):
-            
-        #             if (i not in r) and (j not in c) and (i, j) not in d:
-        #                 lst[i][j] = "Q"
-        #                 r.append(i)
-        #                 c.append(j)
 
 
-        #                 left, right = i, j
-        #                 right2 = j
-        #                 while right < n-1 and left < n-1:
-        #                     left += 1
-        #                     right += 1
-        #                     right2 -= 1
-        #                     d.append((left, right))
-        #                     d.append((left, right2)) 
-                        
-                        
-        #                 backtrack(i, j+1, queens+1)
-        #                 lst[i][j] = "."
 
-        #                 if d:
-        #                     d.pop()
-        #                 r.pop()
-        #                 c.pop()
-        
-        # backtrack(0, 0, 0)
-        # return res
-            
+
+
+
+
+
+
+
+
+
+
+
