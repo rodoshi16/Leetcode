2b13a4792a9b3653e70b87bf34350f11ccd9af59
Rodoshi Mondal <165502326+rodoshi16@users.noreply.github.com>
2026-07-22 17:01:08 -0400

    Time: 41 ms (94.89%), Space: 19.6 MB (86.66%) - LeetHub

--- 0133-clone-graph/0133-clone-graph.py
+++ 0133-clone-graph/0133-clone-graph.py
@@ -10,31 +10,33 @@ from typing import Optional
 class Solution:
     def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
         #input: node
-        # 1-2
+        #1-2
         #deep vs shallow
 
-        #visited set
-        #Node(1) -> Node(2) -> Node(3) -> Node(4)
-        #d: mapping from old to new
+        #Node(1) -> Node(2) -> Node(3) -> node(4)
+        #d: map old to new
 
         d = {}
-        visited = set()
         dummy = Node(0)
-
+        visited = set()
         if node is None:
             return None
 
         def dfs(node): 
             visited.add(node)
+        
             if node not in d:
                 d[node] = Node(node.val)
             
             for nei in node.neighbors:
                 if nei not in visited:
                     dfs(nei)
+                
                 d[node].neighbors.append(d[nei])
-
+            
             dummy.next = d[node]
-        
+
         dfs(node)
         return dummy.next
+
+
