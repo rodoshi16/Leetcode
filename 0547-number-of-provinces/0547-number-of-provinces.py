401551c2c250b6b5a5afdfbc7e59b922e572bad7
Rodoshi Mondal <165502326+rodoshi16@users.noreply.github.com>
2026-07-22 15:27:23 -0400

    Time: 106 ms (5.19%), Space: 25.9 MB (9.25%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.py
+++ 0547-number-of-provinces/0547-number-of-provinces.py
@@ -1,21 +1,24 @@
 class Solution:
     def findCircleNum(self, isConnected: List[List[int]]) -> int:
         #input: adj matrix
-        # output: no of provinces
+
         n = len(isConnected)
         m = len(isConnected[0])
         visited = set()
         c = 0 
 
+        def dfs(i, j):
+            visited.add((i, j))
+            visited.add((j, i))
+
+            for k in range(m):
+                if (j, k) not in visited and isConnected[j][k] == 1:
+                    dfs(j, k)
 
         for i in range(n):
             for j in range(m):
-                if isConnected[i][j] == 1 and (i, j) not in visited:
-                    visited.add((i, j))
-                    visited.add((j, i))
+                if (i, j) not in visited and (j, i) not in visited and isConnected[i][j] == 1:
+                    dfs(i, j) 
                     c += 1
-        
-        return c
 
-
-        
+        return c       
