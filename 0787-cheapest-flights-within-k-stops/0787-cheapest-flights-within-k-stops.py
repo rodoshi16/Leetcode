e51e218a8e5954493fe7649d98c207ffd16b4663
Rodoshi Mondal <165502326+rodoshi16@users.noreply.github.com>
2026-07-29 15:36:19 -0400

    Time: 238 ms (5.55%), Space: 22.4 MB (6.91%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.py
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.py
@@ -3,19 +3,21 @@ from collections import defaultdict
 class Solution:
     def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
 
-        # start at node
-        # append neighbors (price, node, stops)
-        # if node == dst and stops -1 <= k 
+        #constraint - k (no of stops)
+        # (weight, node)
+
+        # k
+        # not checking if no valid path exists
+        # do we need cheapest variable
 
         d = defaultdict(list)
         cheapest = math.inf
 
         for u, v, w in flights:
+            #[1, 100]
             d[u].append([v, w])
         
         seen = {}
-        
-
         minheap = [(0, src, 0)]
 
         while minheap:
@@ -26,20 +28,17 @@ class Solution:
 
             for node, w in d[city]:
                 if (node, stops+1) not in seen or seen[(node, stops+1)] > price + w:
-                    heapq.heappush(minheap, (price + w, node, stops+1))
-                
+                    heapq.heappush(minheap, ((price + w), node, stops+1))
                     seen[(node, stops+1)] = price + w
-                
+
                 if node == dst and stops <= k:
                     cheapest = min(cheapest, price + w)
-
-                
-
+        
+        
         if cheapest == math.inf:
             return -1
         else:
             return cheapest
-
-
+                    
 
 
