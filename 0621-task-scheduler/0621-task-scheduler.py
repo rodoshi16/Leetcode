bf817a37b368178e73d8717f92c33c134d3be014
Rodoshi Mondal <165502326+rodoshi16@users.noreply.github.com>
2026-08-06 16:52:13 -0400

    Time: 198 ms (12.51%), Space: 20.8 MB (37.55%) - LeetHub

---
+++ 0621-task-scheduler/0621-task-scheduler.py
@@ -0,0 +1,35 @@
+import heapq
+from collections import deque
+class Solution:
+    def leastInterval(self, tasks: List[str], n: int) -> int:
+        #find freq and make a max heap
+        # pop from heap and store waiting in queue
+        # if q is ready - put that back in the heap 
+
+        d = {}
+        h = []
+        q = deque([])
+        t = 0
+        for task in tasks:
+            if task not in d:
+                d[task] = 1 
+            else:
+                d[task] += 1
+        
+        for key in d:
+            heapq.heappush(h, (-d[key], key))
+        
+        while h or q:
+            if q and q[0][1] <= t:
+                f, time, ele = q.popleft()
+                heapq.heappush(h, (f, ele))
+                
+            if h:
+                freq, key = heapq.heappop(h)
+                if freq + 1 < 0:
+                    q.append((freq+1, t+n+1, key))
+                
+            
+            t += 1
+        
+        return t
