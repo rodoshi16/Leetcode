86ad1eead26424d1697d195fe3ab05ebeaf2e93a
Rodoshi Mondal <165502326+rodoshi16@users.noreply.github.com>
2026-08-11 12:27:50 -0400

    Time: 0 ms (100%), Space: 19.5 MB (76.95%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
@@ -1,14 +1,52 @@
 class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
-        #m = len(nums2)
-        nums1.extend(nums2)
-        #nlogn + 0(m)
-        nums1.sort()
-        n = len(nums1)
-        i = n // 2
 
-        if n % 2 != 0:
-            return nums1[i]
-        else:
-            return (nums1[i] + nums1[i-1]) / 2
+        n = len(nums1)
+        m = len(nums2)
+        half = (n + m +1) // 2
+
+        if n > m:
+            nums1, nums2 = nums2, nums1
+            n, m = m, n
+        
+        l = 0
+        r = len(nums1)
+
+        while l <= r:
+            i = (l + r) // 2
+            j = half - i 
+
+            #boundary values
+
+            if i == 0:
+                A_left = float('-inf')
+            else:
+                A_left = nums1[i - 1]
+
+            if i == n:
+                A_right = float('inf')
+            else:
+                A_right = nums1[i]
+
+            if j == 0:
+                B_left = float('-inf')
+            else:
+                B_left = nums2[j - 1]
+
+            if j == m:
+                B_right = float('inf')
+            else:
+                B_right = nums2[j]
+
+
+            if A_left > B_right:
+                r = i - 1 
+            elif B_left > A_right:
+                l = i + 1
+            else:
+
+                if (n+m) % 2 != 0:
+                    return max(A_left, B_left)
+                else:
+                    return (max(A_left, B_left) + min(A_right, B_right)) / 2
 
