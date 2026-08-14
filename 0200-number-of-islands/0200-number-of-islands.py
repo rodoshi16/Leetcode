c5bf4b264c10b7e8b7f6eb91b033e48dce223e35
Rodoshi Mondal <165502326+rodoshi16@users.noreply.github.com>
2026-08-14 12:08:55 -0400

    Time: 269 ms (18.53%), Space: 31.3 MB (6.87%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,30 +1,29 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
+        #input: nested list of strings
+        # output: int 
 
-        visited = set()
-        count = 0 
         n = len(grid)
         m = len(grid[0])
+        visited = set()
+        dir = [0, 1], [1, 0], [-1, 0], [0, -1]
+        count = 0
+
 
         def dfs(i, j):
             visited.add((i, j))
-            dir = [[0, 1], [1, 0], [0, -1], [-1, 0]]
 
             for d in dir:
-                a = i + d[0]
-                b = j + d[1]
-                if 0 <= a < n and 0 <= b < m and (a, b) not in visited and grid[a][b] == "1":
+                a = d[0] + i 
+                b = d[1] + j 
+
+                if 0 <= a < n and 0 <= b < m and (a, b) not in visited and grid[a][b] == '1':
                     dfs(a, b)
 
         for i in range(n):
             for j in range(m):
-                if (i, j) not in visited and grid[i][j] == "1":
+                if grid[i][j] == '1' and (i, j) not in visited:
                     dfs(i, j)
-                    count += 1
+                    count += 1 
         
         return count
-
-                
-
-
-        
