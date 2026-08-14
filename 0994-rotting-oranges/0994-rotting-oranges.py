753bf8f722fcd3b411d64c7d70f06424c65d7da4
Rodoshi Mondal <165502326+rodoshi16@users.noreply.github.com>
2026-08-14 12:54:09 -0400

    Time: 4 ms (36.75%), Space: 19.5 MB (12.62%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.py
+++ 0994-rotting-oranges/0994-rotting-oranges.py
@@ -3,46 +3,41 @@ class Solution:
 
         n = len(grid)
         m = len(grid[0])
-
-        minute = 0 
-        visited = set()
+        
+        dir = [0, 1], [1, 0], [0, -1], [-1, 0]
         q = []
-        c = 0 
-        dir = [[0, 1], [1, 0], [-1, 0], [0, -1]]
-
-
+        minute = 0 
+        fresh = 0
         for i in range(n):
             for j in range(m):
                 if grid[i][j] == 2:
                     q.append((i, j))
                 elif grid[i][j] == 1:
-                    c += 1
-        
-        if c == 0:
-            return 0 
-        
-        while q:
-            for _ in range(len(q)):
-                t = q.pop(0)
-                i = t[0]
-                j = t[1]
-                visited.add((i, j))
+                    fresh += 1
 
+        while q and fresh > 0:
+            size = len(q)
+
+            for i in range(size):
+                cell = q.pop(0)
 
                 for d in dir:
-                    a = i + d[0]
-                    b = j + d[1]
+                    a = d[0] + cell[0]
+                    b = d[1] + cell[1]
 
-                    if 0 <= a < n and 0 <= b < m and (a, b) not in visited and grid[a][b] == 1:
+                    if 0 <= a < n and 0 <= b < m and grid[a][b] == 1:
                         grid[a][b] = 2
-                        c -= 1 
+                        fresh -= 1 
                         q.append((a, b))
+                
+            minute += 1 
             
-            minute += 1
-    
-        if c != 0:
-            return -1 
-        else:
-            return minute -1
-                    
-        
+        if fresh > 0:
+            return -1
+        return minute
+
+
+
+
+
+
