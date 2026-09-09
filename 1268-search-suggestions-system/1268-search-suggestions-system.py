48e5d3e0fb34d4eee1b2092dbcb7e376d30694be
Rodoshi Mondal <165502326+rodoshi16@users.noreply.github.com>
2026-09-09 11:28:37 -0400

    Time: 13 ms (66.82%), Space: 22 MB (94.33%) - LeetHub

--- 1268-search-suggestions-system/1268-search-suggestions-system.py
+++ 1268-search-suggestions-system/1268-search-suggestions-system.py
@@ -1,37 +1,29 @@
 class Solution:
     def suggestedProducts(self, products: List[str], searchWord: str) -> List[List[str]]:
-        #after each char is typed, suggest at most three product names from products 
-        #m - mobile, mouse, moneypot, monitor, mousepad
-        #m - mobile, moneypot, monitor
-        #mo - moneypot, monitorm, mouse
-        #mou - mouse, mousepad
-
-
+        
         products.sort()
         lst = []
 
         for i in range(len(searchWord)):
+            prefix = searchWord[:i+1]
+
+            l = 0
+            r = len(products) - 1
+
+            while l <= r:
+                mid = (l + r) // 2
+
+                if products[mid][:i+1] < prefix:
+                    l = mid + 1
+                else:
+                    r = mid - 1
+
             t = []
-            for ele in products:
-                if searchWord[0:i+1] == ele[0:i+1] and len(t) < 3:
-                    t.append(ele)
-            
+
+            for j in range(l, min(l + 3, len(products))):
+                if products[j][:i+1] == prefix:
+                    t.append(products[j])
+
             lst.append(t)
-        
+
         return lst
-                
-        
-
-            
-
-            
-
-
-
-
-
-
-
-
-
-        
